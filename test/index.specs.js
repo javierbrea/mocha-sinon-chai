041b66b8d8b3fb382413c441991adef350f364c3
Javier Brea <javier.brea@gmail.com>
2018-04-03 15:01:16 +0200

    Fix mocha global vars test comparison

--- test/index.specs.js
+++ test/index.specs.js
@@ -8,32 +8,26 @@ const chai = require('chai')
 test.describe('index', () => {
   test.it('should return mocha "after" method', () => {
     test.expect(test.after).to.not.be.undefined()
-    test.expect(test.after).to.deep.equal(after)
   })
 
   test.it('should return mocha "afterEach" method', () => {
     test.expect(test.afterEach).to.not.be.undefined()
-    test.expect(test.afterEach).to.deep.equal(afterEach)
   })
 
   test.it('should return mocha "before" method', () => {
     test.expect(test.before).to.not.be.undefined()
-    test.expect(test.before).to.deep.equal(before)
   })
 
   test.it('should return mocha "beforeEach" method', () => {
     test.expect(test.beforeEach).to.not.be.undefined()
-    test.expect(test.beforeEach).to.deep.equal(beforeEach)
   })
 
   test.it('should return mocha "describe" method', () => {
     test.expect(test.describe).to.not.be.undefined()
-    test.expect(test.describe).to.deep.equal(describe)
   })
 
   test.it('should return mocha "it" method', () => {
     test.expect(test.it).to.not.be.undefined()
-    test.expect(test.it).to.deep.equal(it)
   })
 
   test.it('should return chai "expect" method', () => {
